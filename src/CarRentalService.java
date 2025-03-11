e1092c8d0cf547379517683752106cf327b5b9c1
Abir Saha <abirsaha453@gmail.com>
2025-03-11 12:46:10 +0530

    Hustlers | task done

--- src/CarRentalService.java
+++ src/CarRentalService.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,12 +12,41 @@ public class CarRentalService {
 //    hardcode data
 
     CarRentalService() {
-        this.cars = new ArrayList<Car>();
+        this.cars =getCars();
         this.customers = new ArrayList<Customer>();
     }
 
 
+public List<Car> getCars() {
+    File file=new File("src/Cars.txt");
+    List<Car> ALlCars=new ArrayList<>();
+    try(FileReader CarsReader=new FileReader(file)){
 
+        BufferedReader br=new BufferedReader(CarsReader);
+        String readingCars;
+        String id,make,model;
+        int year;
+        boolean available;
+        while((readingCars=br.readLine())!=null){
+            String[] arr =readingCars.split(",");
+            id=arr[0];
+            make=arr[1];
+            model=arr[2];
+            year=Integer.parseInt(arr[3]);
+            available=Boolean.parseBoolean(arr[4]);
+
+            Car car=new Car(id,make,model,year,available);
+            ALlCars.add(car);
+//            System.out.println(car.getAvailable()+car.getMake()+car.getModel()+car.getYear());
+
+        }
+
+
+    }catch(IOException e){
+        System.out.println(e.getMessage());
+    }
+    return  ALlCars;
+}
 
     public List<Car> allCars(){
         return  cars;
@@ -33,7 +66,7 @@ public class CarRentalService {
     public List<Car> getAvailableCars(){
         List<Car> availableCars = new ArrayList<>();
         for (Car car:cars){
-            System.out.println(car.getAvailable());
+//            System.out.println(car.getAvailable());
             if(car.getAvailable())availableCars.add(car);
         }
         return availableCars;
--- src/CarRentalSystem.java
+++ src/CarRentalSystem.java
@@ -1,23 +1,28 @@
+import java.io.*;
 import java.util.List;
 import java.util.Scanner;
-
+class CustomerExistsException extends Exception {
+    public CustomerExistsException(String message) {
+        super(message);
+    }
+}
 public class CarRentalSystem {
+
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
         System.out.println("welcome to our CarRentalSystem");
-        System.out.println("Enter customer details below");
-        System.out.println("Enter customerID");
-        String customerID=input.nextLine();
-        System.out.println("Enter customer name");
-        String customerName=input.nextLine();
-        Customer customer=new  Customer(customerID,customerName);
+        Scanner input = new Scanner(System.in);
+
         CarRentalService carRentalService = new CarRentalService();
-        carRentalService.addCustomer(customer);
+//        carRentalService.addCustomer(customer);
         System.out.println("choose your action from the below list");
         System.out.println("1. Create a new car");
         System.out.println("2. Rent a car");
-        System.out.println("3. View all cars");
+//        System.out.println("3. View all cars");
 //        System.out.println("4. view list of specific user ka class");
+        System.out.println("3.Register");
+
 
 
         Car car1=new Car("1","hyundai","verna",2024,true);
@@ -46,6 +51,10 @@ public class CarRentalSystem {
 
 
 
+
+
+
+
         switch(choice){
             case 1:
                 System.out.println("enter the car details below");
@@ -62,7 +71,23 @@ public class CarRentalSystem {
                 boolean isAvailable=input.nextBoolean();
                 Car car=new  Car(carID,make,model,year,isAvailable);
                 carRentalService.addCar(car);
-                System.out.println("The car has been addded successfully");
+                File CarFile=new File("src/Cars.txt");
+                if(CarFile.exists()){
+                    try (FileWriter Carwriter = new FileWriter(CarFile,true)){
+
+                        BufferedWriter carwriter=new BufferedWriter(Carwriter);
+
+                        carwriter.write(car.getCarId()+","+make+","+model+","+year+","+isAvailable);
+                        carwriter.newLine();
+                        carwriter.flush();
+                        carwriter.close();
+                        System.out.println("Car has been added to the System successfully.");
+
+                    }catch (IOException e){
+                        System.out.println("An error occurred while writing to the Car file "
+                                + " to the file: " + e.getMessage());
+                    }
+                }
                 break;
             case 2:
 
@@ -76,15 +101,72 @@ public class CarRentalSystem {
                 System.out.println("enter the index of the car you want to rent");
                 int index=input.nextInt();
                 Car carToRent=available.get(index-1);
-                customer.rentCar(carToRent);
+//                idhar kuch karna padedga cstomer hardcoded hai
+                customer1.rentCar(carToRent);
                 System.out.println("The car " + carToRent.getModel()+ " " + carToRent.getMake() + " "+ carToRent.getYear()+ " has been rented successfully");
                 break;
             case 3:
-                System.out.println("All the cars that are being listed here are ");
-                List<Car> allCars=carRentalService.allCars();
-                for(int i=0;i<allCars.size();i++){
-                    System.out.println((i+1)+". "+allCars.get(i).getModel()+" "+allCars.get(i).getYear()+" "+allCars.get(i).getMake());
+
+                System.out.println("Enter customer details below");
+                System.out.println("Enter customerID");
+                input.nextLine();
+                String customerID=input.nextLine();
+                System.out.println("the customer id is "+customerID);
+                File CustomerIDFile=new File("src/CustomerID.txt");
+                if(CustomerIDFile.exists()){
+                    System.out.println("hey i am here ");
+                    try(FileReader CustomerIDReader=new FileReader(CustomerIDFile)){
+                        BufferedReader customerIDReader=new BufferedReader(CustomerIDReader);
+                        String line;
+                        while((line=customerIDReader.readLine())!=null){
+                            if(line.equals(customerID)) {
+                                System.out.println("The customer " + customerID + " already exists.");
+                                throw new CustomerExistsException("The customer with this id already exists try another time with a diff id or if it is you login donot register dumbo!");
+                            }
+
+                        }
+                        try(FileWriter CustomerIDWriter=new FileWriter(CustomerIDFile,true)){
+                            BufferedWriter  customerIDWriter=new BufferedWriter(CustomerIDWriter);
+                            customerIDWriter.flush();
+                            customerIDWriter.write(customerID);
+                            customerIDWriter.newLine();
+                            customerIDWriter.flush();
+                            customerIDWriter.close();
+                            System.out.println("The customer " + customerID + " is new has been noted successfully");
+                        }
+                        System.out.println("Enter customer name");
+                        String customerName=input.nextLine();
+//                Customer customer=new  Customer(customerID,customerName)
+
+                        File CustomerFile=new File("src/Customers.txt");
+                        if(CustomerFile.exists()) {
+                            try (FileWriter CustomerWriter = new FileWriter(CustomerFile, true)) {
+
+                                BufferedWriter customerwriter = new BufferedWriter(CustomerWriter);
+                                customerwriter.flush();
+                                customerwriter.write(customerID + "," + customerName);
+                                customerwriter.newLine();
+                                customerwriter.flush();
+                                customerwriter.close();
+                                System.out.println("Customer has been registered successfully.");
+
+
+                            } catch (IOException e) {
+                                System.out.println("there was an error writing into the  Customer file " + e.getMessage());
+                            }
+                        }
+
+                        }catch(IOException e){
+                        System.out.println("An error occurred while Reading to the CustomerID file ");
+                    }catch(CustomerExistsException e){
+                        System.out.println("This error occured while signing up the customer "+ e.getMessage());
+                    }
+
                 }
+
+
+
+
 
                 break;
 
